d4cc279ff3c23b5667c69c5854ed337e6392ad00
test <admin@test.com>
2024-09-18 21:07:23 +0800

    first

--- main.java
+++ main.java
@@ -1,5 +1,20 @@
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello World");
+        //第一个循环
+        for (int i = 0; i < 5; i++)
+        {
+            System.out.println("i = " + i);
+        }
+        //输出一首古诗
+        System.out.println("床前明月光");
+        System.out.println("疑是地上霜");
+        System.out.println("举头望明月");
+        System.out.println("低头思故乡");
+
+
+
     }
+
 }
